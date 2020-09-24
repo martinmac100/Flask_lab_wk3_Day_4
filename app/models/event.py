fb758dc34fce4735c33c60c1c8b364cc3636d9fd
matinmac100 <matinmac100@yahoo.co.uk>
2020-09-24 14:57:51 +0100

    added events

---
+++ app/models/event.py
@@ -0,0 +1,8 @@
+class Event():
+
+    def __init__(self, date, name, num_guests, room, description):
+        self.date = date
+        self.name = name
+        self.num_guests = num_guests
+        self.room = room
+        self.description = description
---
+++ app/models/event_list.py
@@ -0,0 +1,7 @@
+from app.models.event import *
+
+event1 = Event("24.12.2020", "Christmas Party", 20, "Party Room", "Dinner")
+event2 = Event("31.10.2020", "Halloween", 40, "Scary Room", "Fright Night")
+
+events = [event1, event2]
+
